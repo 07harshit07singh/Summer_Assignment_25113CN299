d86090fa8715a674c4c89203e3affb4bcad995f4
07harshit07singh <07harshit07singh@gmail.com>
2026-06-08 16:46:26 +0530

    Day8 assignment is uploaded

---
+++ Day8/ques31.c
@@ -0,0 +1,24 @@
+/* Program to print character triangle.
+A
+AB
+ABC
+ABCD
+ABCDE
+*/ 
+
+#include <stdio.h>
+int main() {
+    int n, i, j;
+    printf("Enter the number of rows : ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%c", 'A' + j - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
