15cdfe748ecd568d960f40a5886e8caaaaaa8581
07harshit07singh <07harshit07singh@gmail.com>
2026-06-10 21:50:28 +0530

    Day9 assignment is uploaded

---
+++ Day9/ques35.c
@@ -0,0 +1,24 @@
+/* Program to print repeated character pattern.
+A
+BB
+CCC
+DDDD
+EEEEE
+*/ 
+
+#include <stdio.h>
+int main() {
+    int n, i, j;
+    printf("Enter the number of rows : ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%c", 'A' + i - 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
