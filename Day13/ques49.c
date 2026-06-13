ff4a5b12c5954fee8f1c1cdeb795ad113256dcff
07harshit07singh <07harshit07singh@gmail.com>
2026-06-13 22:40:29 +0530

    Day13 assignment is uploaded

---
+++ Day13/ques49.c
@@ -0,0 +1,23 @@
+// Program to input and display array.
+
+#include <stdio.h>
+int main() {
+    int arr[100], n, i;
+    printf("Enter number of elements : ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements :\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]); 
+    }
+
+    printf("Array elements are :\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
---
+++ Day13/ques51.c
@@ -0,0 +1,34 @@
+// Program to find largest and smallest element.
+
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, largest, smallest;
+    printf("Enter the number of elements :\n");
+    scanf("%d", &n);
+
+    printf("Enter %d elements :\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+    smallest = arr[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+
+        if(arr[i] < smallest)
+        {
+            smallest = arr[i];
+        }
+    }
+    printf("Largest element = %d\n", largest);
+    printf("Smallest element = %d\n", smallest);
+    return 0;
+}
